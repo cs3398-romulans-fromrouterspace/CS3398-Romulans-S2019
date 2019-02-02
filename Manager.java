40a4fa30c2634db76d87ce4547cea4c3c393ca1e
Anne <anne.dromeda.0112@gmail.com>
2019-02-02 16:25:08 -0600

    Create Manager.java
    Added Manager class.  Will update after other files loaded to integrate them all.

---
+++ Manager.java
@@ -0,0 +1,16 @@
+package threesolid;
+
+public class Manager {
+	IWorker worker;
+
+	public void Manager() {
+
+	}
+	public void setWorker(IWorker w) {
+		worker=w;
+	}
+
+	public void manage() {
+		worker.work();
+	}
+}
