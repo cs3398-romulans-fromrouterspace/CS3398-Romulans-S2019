7fedb254f0578b3270945d560a9e7bb03b6a74f7
Anne <anne.dromeda.0112@gmail.com>
2019-02-02 19:25:39 -0600

    Update Manager.java
    Per assignment instructions, added notes to describe how 3 principles could be applied to manager class.

--- Manager.java
+++ Manager.java
@@ -1,3 +1,19 @@
+/* Class Manager.  Supposed to a subset of Worker.  
+
+Open-Close Principle: if we added a new position to basic worker,
+such as Manager or Auditor, we could simply extend existing code, rather than
+modifying the Worker class itself.
+
+Inteface Segragation:  The manager class would have tasks specific to the duties
+of management, rather than just cramming manager methods into the Worker class,
+which non-manager workers would not use.
+
+Single Responsibility:  the developer-oriented counterpart to Interface
+Segregation.  Again, the Manager class would focus exlusively on managerial
+duties and the programmed implementations of those duties.
+
+*/
+
 package threesolid;
 
 public class Manager {
